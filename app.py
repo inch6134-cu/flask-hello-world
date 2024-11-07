af413150656de21e61965db44aad6a65f7b852ec
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-11-06 19:40:49 -0700

    Fixed '/db_test' route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from Alex Chang in 3308!'
 
-@app.route('db_test')
+@app.route('/db_test')
 def db_test():
     conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a/flask_hello_world_db")
     conn.close()
