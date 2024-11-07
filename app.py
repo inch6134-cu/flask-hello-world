89fe70ecb3aa48766f393757210d7a864096758e
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-11-06 19:31:49 -0700

    Added PostgreSQL database connection

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Alex Chang in 3308!'
+
+@app.route('db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a/flask_hello_world_db")
+    conn.close()
+    return "Database connection successful"
