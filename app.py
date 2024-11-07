09dee987c793bba6596c8897375ade378e9b6488
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-11-06 19:54:50 -0700

    Added db_create route

--- app.py
+++ app.py
@@ -9,6 +9,29 @@ def hello_world():
 
 @app.route('/db_test')
 def db_test():
+    # INTERNAL 
     conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a/flask_hello_world_db")
+    # EXTERNAL
+    # conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a.oregon-postgres.render.com/flask_hello_world_db")
     conn.close()
     return "Database connection successful"
+
+@app.route('/db_create')
+def db_create():
+    # INTERNAL 
+    conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a/flask_hello_world_db")
+    # EXTERNAL
+    # conn = psycopg2.connect("postgresql://flask_hello_world_db_user:dlmqZKPSo3masMFcKkwZwEmLO5mgFfUT@dpg-csm2b2jtq21c738b8vk0-a.oregon-postgres.render.com/flask_hello_world_db")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfully created"
